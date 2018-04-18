cce1e11b77c597119b71954629af6928170fcd7c
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-04-18 18:57:49 +0530

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -7,10 +7,10 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-echo "================= Installing nodejs 9.11.1 ==================="
+echo "================= Installing nodejs 9.11* ==================="
 curl --silent --location https://rpm.nodesource.com/setup_9.x | sudo bash -
 sudo yum install nodejs
 
-echo "================= Installing yarn 1.5.1 ==================="
+echo "================= Installing yarn 1.5* ==================="
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 sudo yum install yarn
