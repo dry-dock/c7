f143a16f3956898c441d87b3b4cff976705cf7de
Devashish <devashish.86@gmail.com>
2018-04-19 14:52:04 +0530

    fixign java and javac paths

--- java/install.sh
+++ java/install.sh
@@ -23,11 +23,9 @@ wget --no-cookies \
   --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
   "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.rpm"
 
-sleep 3
-
 sudo yum localinstall -y jdk-8u171-linux-x64.rpm
-sudo update-alternatives --set java /usr/java/jre1.8.0_171/bin/java
-sudo update-alternatives --set javac /usr/java/jdk1.8.0_171/bin/javac
+sudo update-alternatives --set java /usr/java/jre1.8.0_171-amd64/bin/java
+sudo update-alternatives --set javac /usr/java/jdk1.8.0_171-amd64/bin/javac
 rm jre-8u171-linux-x64.rpm
 rm jdk-8u171-linux-x64.rpm
 echo 'export JAVA_HOME=/usr/java/jdk1.8.0_171' >> /etc/drydock/.env
