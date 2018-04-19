40ae42f1b703c02611042e692dd5ca6248d82bc7
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-04-19 17:07:00 +0530

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -9,10 +9,12 @@ export NVM_DIR="$HOME/.nvm"
 
 echo "================= Installing nodejs 9.11* ==================="
 curl --silent --location https://rpm.nodesource.com/setup_9.x | sudo bash -
+#adding key required to install nodejs
 rpm --import /etc/pki/rpm-gpg/NODESOURCE-GPG-SIGNING-KEY-EL
 sudo yum install nodejs
 
 echo "================= Installing yarn 1.5* ==================="
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+#adding key required to install yarn
 rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
 sudo yum install yarn
