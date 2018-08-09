56bb9f9fbac1deb81d6fa2f3ed9f2425f526df9e
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-08-09 16:09:37 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -e
 
-echo "================= Installing openjdk-8-jdk ==================="
-sudo yum install -y java-1.8.0-openjdk-devel
+echo "================= Installing openjdk-10-jdk ==================="
+cd /usr/lib/jvm
+wget "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz"
+tar -xzf openjdk-10.0.2_linux-x64_bin.tar.gz
+mv jdk-10.0.2/ java-10-openjdk-amd64
+
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-10-openjdk-amd64/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac 1
+sudo update-alternatives --set java /usr/lib/jvm/java-10-openjdk-amd64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-10-openjdk-amd64/bin/javac
+
+echo 'export JAVA_HOME=/usr/lib/jvm/java-10-openjdk-amd64' >> /etc/drydock/.env
+echo 'export PATH="$PATH:/usr/lib/jvm/java-10-openjdk-amd64/bin/java/bin' >> /etc/drydock/.env
 
 
-echo "================ Installing oracle-java8-installer ================="
+echo "================ Installing oracle-java10-installer ================="
 
 echo "========= Installing JRE =============="
 wget --no-cookies \
@@ -12,8 +23,8 @@ wget --no-cookies \
   --retry-connrefused \
   --tries=5 \
   --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-  "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jre-8u171-linux-x64.rpm"
-sudo yum localinstall -y jre-8u171-linux-x64.rpm
+  "http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/jre-10.0.2_linux-x64_bin.rpm"
+sudo yum localinstall -y jre-10.0.2_linux-x64_bin.rpm
 
 echo "========= Installing JDK =============="
 wget --no-cookies \
@@ -21,12 +32,14 @@ wget --no-cookies \
   --retry-connrefused \
   --tries=5 \
   --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-  "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.rpm"
+  "http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/jdk-10.0.2_linux-x64_bin.rpm"
 
-sudo yum localinstall -y jdk-8u171-linux-x64.rpm
-sudo update-alternatives --set java /usr/java/jre1.8.0_171-amd64/bin/java
-sudo update-alternatives --set javac /usr/java/jdk1.8.0_171-amd64/bin/javac
-rm jre-8u171-linux-x64.rpm
-rm jdk-8u171-linux-x64.rpm
-echo 'export JAVA_HOME=/usr/java/jdk1.8.0_171-amd64' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/java/jre1.8.0_171-amd64/bin' >> /etc/drydock/.env
+sudo yum localinstall -y jdk-10.0.2_linux-x64_bin.rpm
+sudo update-alternatives --set java /usr/java/jdk-10.0.2/bin/java
+sudo update-alternatives --set javac /usr/java/jdk-10.0.2/bin/javac
+
+rm jre-10.0.2_linux-x64_bin.rpm
+rm jdk-10.0.2_linux-x64_bin.rpm
+
+echo 'export JAVA_HOME=/usr/java/jdk-10.0.2' >> /etc/drydock/.env
+echo 'export PATH=$PATH:/usr/java/jre-10.0.2/bin' >> /etc/drydock/.env
