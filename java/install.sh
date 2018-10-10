1a4370ebc15d7d98ee7fabeedb3f79e13b0d05c9
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-10-10 13:37:33 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -12,8 +12,8 @@ sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-"$OPEN
 sudo update-alternatives --set java /usr/lib/jvm/java-"$OPENJDK_VERSION"-openjdk-amd64/bin/java
 sudo update-alternatives --set javac /usr/lib/jvm/java-"$OPENJDK_VERSION"-openjdk-amd64/bin/javac
 
-echo 'export JAVA_HOME=/usr/lib/jvm/java-"$OPENJDK_VERSION"-openjdk-amd64' >> /etc/drydock/.env
-echo 'export PATH="$PATH:/usr/lib/jvm/java-"$OPENJDK_VERSION"-openjdk-amd64/bin/java/bin"' >> /etc/drydock/.env
+echo "export JAVA_HOME=/usr/lib/jvm/java-"$OPENJDK_VERSION"-openjdk-amd64" >> /etc/drydock/.env
+echo "export PATH=$PATH:/usr/lib/jvm/java-"$OPENJDK_VERSION"-openjdk-amd64/bin/java/bin" >> /etc/drydock/.env
 
 
 export ORACLEJDK_VERSION=11
@@ -31,5 +31,5 @@ sudo update-alternatives --set javac /usr/java/jdk-"$ORACLEJDK_VERSION"/bin/java
 
 rm jdk-"$ORACLEJDK_VERSION"_linux-x64_bin.rpm
 
-echo 'export JAVA_HOME=/usr/java/jdk-"$ORACLEJDK_VERSION"' >> /etc/drydock/.env
-echo 'export PATH=$PATH:/usr/java/jdk-"$ORACLEJDK_VERSION"/bin' >> /etc/drydock/.env
+echo "export JAVA_HOME=/usr/java/jdk-"$ORACLEJDK_VERSION"" >> /etc/drydock/.env
+echo "export PATH=$PATH:/usr/java/jdk-"$ORACLEJDK_VERSION"/bin" >> /etc/drydock/.env
