2ec28976f9cb62254368663cf9d12788c4431e79
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-02-13 10:19:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ sudo yum install -y \
   python36u-devel \
   python36u-pip
 
-export VIRTUALENV_VERSION=16.2.0
+export VIRTUALENV_VERSION=16.4.0
 echo "================= Adding $VIRTUALENV_VERSION ==================="
 sudo pip install virtualenv=="$VIRTUALENV_VERSION"
 
@@ -94,7 +94,7 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.s
 sudo yum install git-lfs-"$GITLFS_VERSION"
 git lfs install
 
-export GCLOUD_SDKREPO=228.0*
+export GCLOUD_SDKREPO=233.0*
 echo "================= Adding gcloud "$GCLOUD_SDKREPO"  ============"
 sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
 [google-cloud-sdk]
@@ -112,26 +112,26 @@ rpm --import  https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 sudo yum install -y google-cloud-sdk-"$GCLOUD_SDKREPO"
 
 
-KUBECTL_VERSION=v1.13.1
+KUBECTL_VERSION=v1.13.2
 echo "================= Adding kubectl "$KUBECTL_VERSION" ==================="
 curl -sSLO https://storage.googleapis.com/kubernetes-release/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-KOPS_VERSION=1.10.1
+KOPS_VERSION=1.11.1
 echo "Installing KOPS version: $KOPS_VERSION"
 curl -LO https://github.com/kubernetes/kops/releases/download/"$KOPS_VERSION"/kops-linux-amd64
 sudo chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
 
-HELM_VERSION=v2.12.1
+HELM_VERSION=v2.12.3
 echo "Installing helm version: $HELM_VERSION"
 wget https://storage.googleapis.com/kubernetes-helm/helm-"$HELM_VERSION"-linux-amd64.tar.gz
 tar -zxvf helm-"$HELM_VERSION"-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
 rm -rf linux-amd64
 
-export AWS_VERSION=1.16.81
+export AWS_VERSION=1.16.102
 echo "================= Adding awscli "$AWS_VERSION" ============"
 sudo pip install  awscli=="$AWS_VERSION"
 
@@ -146,20 +146,20 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 sudo yum install azure-cli-$AZURE_CLI_VERSION
 
-export DOCTL_VERSION=1.12.2
+export DOCTL_VERSION=1.13.0
 echo "================= Adding doctl $DOCTL_VERSION============"
 curl -OL https://github.com/digitalocean/doctl/releases/download/v"$DOCTL_VERSION"/doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
 tar xf doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
 sudo mv doctl /usr/local/bin
 rm doctl-"$DOCTL_VERSION"-linux-amd64.tar.gz
 
-export JFROG_VERSION=1.23.1
+export JFROG_VERSION=1.24.1
 echo "================= Adding jfrog-cli "$JFROG_VERSION"==================="
 wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
 sudo chmod +x jfrog
 sudo mv jfrog /usr/bin/jfrog
 
-export ANSIBLE_VERSION=2.7.5
+export ANSIBLE_VERSION=2.7.7
 echo "================ Adding ansible $ANSIBLE_VERSION===================="
 sudo pip install ansible=="$ANSIBLE_VERSION"
 
@@ -167,7 +167,7 @@ export BOTO_VERSION=2.49.0
 echo "================ Adding boto $BOTO_VERSION ======================="
 sudo pip install  boto=="$BOTO_VERSION"
 
-export BOTO3_VERSION=1.9.71
+export BOTO3_VERSION=1.9.92
 echo "============  Adding boto3 "$BOTO_VERSION" ==============="
 sudo pip install boto3=="$BOTO3_VERSION"
 
@@ -207,7 +207,7 @@ mv /tmp/terraform/terraform /usr/bin/terraform
 echo "Added terraform successfully"
 echo "-----------------------------------"
 
-export PK_VERSION=1.3.3
+export PK_VERSION=1.3.4
 echo "================ Adding packer $PK_VERSION ===================="
 export PK_FILE=packer_"$PK_VERSION"_linux_amd64.zip
 
